d22dc00df53dbe7c5ff587a61eb869fac17189f1
Malik Baktiyarov <malikbakt@gmail.com>
2012-05-03 07:38:18 +0400

    mixed games

--- src/game.erl
+++ src/game.erl
@@ -2,30 +2,68 @@
 -record(game, {id, type, limit}).
 
 %% types
--define(HOLDEM, 0). %% community cards poker
--define(SEVEN_CARD, 1). %% 7 card poker
--define(DRAW, 2). %% draw poker
+-define(HOLDEM, 1). %% community cards poker
+-define(SEVEN_CARD, 2). %% 7 card poker
+-define(DRAW, 3). %% draw poker
+-define(MIX, 4). %% draw poker
 
 %% limits
--define(NO_LIMIT, 0). %% NL
--define(POT_LIMIT, 1). %% PL
--define(FIXED_LIMIT, 2). %% FL
+-define(NO_LIMIT, 1). %% NL
+-define(POT_LIMIT, 2). %% PL
+-define(FIXED_LIMIT, 3). %% FL
 
 %% games
--define(TEXAS, 0).
--define(OMAHA, 1).
--define(OMAHA8, 2).
--define(STUD, 3).
--define(STUD8, 4).
--define(RAZZ, 5).
--define(LONDON, 6).
--define(FIVE_CARD, 7).
--define(SINGLE27, 8).
--define(TRIPLE27, 9).
--define(BADUGI, 10).
+%% holdem
+-define(TEXAS, 1).
+-define(OMAHA, 2).
+-define(OMAHA8, 3).
+%% seven card
+-define(STUD, 4).
+-define(STUD8, 5).
+-define(RAZZ, 6).
+-define(LONDON, 7).
+%% draw
+-define(FIVE_CARD, 8).
+-define(SINGLE27, 9).
+-define(TRIPLE27, 10).
+-define(BADUGI, 11).
+%% mixes
+-define(HORSE, 12).
+-define(HOSE, 13).
+-define(MIX7, 14).
+-define(MIX8, 15).
+-define(MIX9, 16).
+-define(MIXED_HOLDEM, 17).
+-define(MIXED_STUD, 18).
+-define(MIXED_DRAW, 19).
 
 %% new_game(?TEXAS, ?HOLDEM, ?NO_LIMIT).
-new(Game, Type, Limit) -> #game{id = Game, type = Type, limit = Limit}.
+new(Type, Limit) -> #game{type = Type, limit = Limit}.
+
+mix(Type) when Type == ?HORSE ->
+  {[?HOLDEM, ?OMAHA8, ?RAZZ, ?STUD, ?STUD8], ?FIXED_LIMIT};
+mix(Type) when Type == ?HOSE ->
+  {[?HOLDEM, ?OMAHA8, ?STUD, ?STUD8], ?FIXED_LIMIT};
+mix(Type) when Type == ?MIX8 -> %% 8-game
+  [
+    {[?TRIPLE27, ?HOLDEM, ?OMAHA8, ?RAZZ, ?STUD, ?STUD8], ?FIXED_LIMIT},
+    {?HOLDEM, ?NO_LIMIT},
+    {?OMAHA, ?POT_LIMIT}
+  ];
+mix(Type) when Type = ?MIX9 ->
+  [
+    {[?BADUGI, ?TRIPLE27, ?HOLDEM, ?OMAHA8, ?RAZZ, ?STUD, ?STUD8], ?FIXED_LIMIT},
+    {?HOLDEM, ?NO_LIMIT},
+    {?OMAHA, ?POT_LIMIT}
+  ];
+mix(Type) when Type == ?MIXED_HOLDEM ->
+  [{?HOLDEM, ?FIXED_LIMIT}, {?HOLDEM, ?NO_LIMIT}, {?OMAHA8, ?FIXED_LIMIT}, {?OMAHA, ?POT_LIMIT}];
+mix(Type) when Type == ?MIXED_STUD ->
+  {[?STUD, ?STUD8, ?RAZZ], ?FIXED_LIMIT};
+mix(Type) when Type == ?MIX7 ->
+  {[?STUD, ?STUD8, ?RAZZ, ?LONDON], ?FIXED_LIMIT},
+mix(Type) when Type == ?MIXED_DRAW ->
+  {[?BADUGI, ?FIVE_CARD, ?SINGLE27, ?TRIPLE27], ?FIXED_LIMIT}.
 
 globals() ->
   [
@@ -86,59 +124,59 @@ defaults(Game) when Game#game.type == ?DRAW ->
     {table_max, 6},
     {reshuffle, true}
   ].
-options(Game) when Game#game.id == ?TEXAS ->
+options(Game) when Game#game.type == ?TEXAS ->
   [
     {ranking, fun hand:high_card/1},
     {hole_cards, 2}
   ];
-options(Game) when Game#game.id == ?OMAHA ->
+options(Game) when Game#game.type == ?OMAHA ->
   [
     {ranking, fun hand:high_card/1},
     {hole_cards, 4},
     {limit, ?POT_LIMIT}
   ];
-options(Game) when Game#game.id == ?OMAHA8 ->
+options(Game) when Game#game.type == ?OMAHA8 ->
   [
     {ranking, fun hand:high_card/1},
     {ranking, fun hand:ace5_low8/1},
     {hole_cards, 4},
     {limit, ?POT_LIMIT}
   ];
-options(Game) when Game#game.id == ?STUD ->
+options(Game) when Game#game.type == ?STUD ->
   [
     {ranking, fun hand:high_card/1}
   ];
-options(Game) when Game#game.id == ?STUD8 ->
+options(Game) when Game#game.type == ?STUD8 ->
   [
     {ranking, fun hand:high_card/1},
     {ranking, fun hand:ace5_low8/1}
   ];
-options(Game) when Game#game.id == ?RAZZ ->
+options(Game) when Game#game.type == ?RAZZ ->
   [
     {ranking, fun hand:ace5_low/1}
   ];
-options(Game) when Game#game.id == ?LONDON ->
+options(Game) when Game#game.type == ?LONDON ->
   [
     {ranking, fun hand:ace6_low/1}
   ];
-options(Game) when Game#game.id == ?FIVE_CARD ->
+options(Game) when Game#game.type == ?FIVE_CARD ->
   [
     {ranking, fun hand:high_card/1},
     {hole_cards, 5}
   ];
-options(Game) when Game#game.id == ?SINGLE27 ->
+options(Game) when Game#game.type == ?SINGLE27 ->
   [
     {ranking, fun hand:deuce7_low/1},
     {hole_cards, 5},
     {streets, 1}
   ];
-options(Game) when Game#game.id == ?TRIPLE27 ->
+options(Game) when Game#game.type == ?TRIPLE27 ->
   [
     {ranking, fun hand:deuce7_low/1},
     {hole_cards, 5},
     {streets, 3}
   ];
-options(Game) when Game#game.id == ?BADUGI ->
+options(Game) when Game#game.type == ?BADUGI ->
   [
     {ranking, fun hand:badugi_hand/1},
     {hole_cards, 4},
