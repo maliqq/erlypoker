7de4a883939b1560011ac3970d1216aee974b812
Malik Baktiyarov <malikbakt@gmail.com>
2012-05-03 05:14:26 +0400

    ...

--- poker.hrl
+++ poker.hrl
@@ -1,5 +1,12 @@
 -record(card, {kind, suit}).
 -record(card_group, {kind = none, suit = none, value}).
 
+-record(bet, {
+    amount,
+    call = false,
+    fold = false,
+    forced = false
+  }).
+
 -define(SUITS, ["s", "h", "d", "c"]).
 -define(KINDS, ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]).
