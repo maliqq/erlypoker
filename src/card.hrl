eabb32a3a0a89a0181db7cb2f0313e657d3aca7e
Malik Baktiyarov <malik@undev.ru>
2012-05-28 19:35:43 +0400

    card macros

--- src/card.hrl
+++ src/card.hrl
@@ -1,7 +1,19 @@
 -define(kinds(), lists:seq(0, 12)).
 -define(suits(), lists:seq(0, 3)).
--define(card(Kind, Suit), (Kind bsl 2 + Suit)).
--define(suit(Mask), (Mask & 2#11)).
--define(kind(Mask), (Mask bsr 2)).
 -define(cards(), [card(Kind, Suit) || Kind <- kinds(), Suit <- suits()]).
 
+-define(kind(Mask), (Mask bsr 2)).
+-define(suit(Mask), (Mask & 2#11)).
+-define(card(Kind, Suit), (Kind bsl 2 + Suit)).
+
+-define(card_chars(), [$2, $3, $4, $5, $6, $7, $8, $9, $T, $J, $Q, $K, $A]).
+-define(card_names(), ["deuce", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"]).
+
+-define(suit_chars(), ["s", "h", "d", "c"]).
+-define(suit_chars_utf8(), [
+    <<"♠"/utf8>>,
+    <<"♥"/utf8>>,
+    <<"♦"/utf8>>,
+    <<"♣"/utf8>>
+  ]).
+-define(suit_names(), ["spade", "heart", "diamond", "club"]).
