7557cda01cb06dd0bbe2c935d515be9f5b093e20
Malik Baktiyarov <malik@undev.ru>
2012-05-28 19:10:41 +0400

    card mask

--- src/card.hrl
+++ src/card.hrl
@@ -1,7 +1,7 @@
 -define(kinds(), lists:seq(0, 12)).
 -define(suits(), lists:seq(0, 3)).
--define(mask(Kind, Suit), (Kind bsl 2 + Suit)).
+-define(card(Kind, Suit), (Kind bsl 2 + Suit)).
 -define(suit(Mask), (Mask & 2#11)).
 -define(kind(Mask), (Mask bsr 2)).
--define(cards(), [mask(Kind, Suit) || Kind <- kinds(), Suit <- suits()]).
+-define(cards(), [card(Kind, Suit) || Kind <- kinds(), Suit <- suits()]).
 
