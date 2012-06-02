4fc3324521130d931b73e9111a2568c7117697d5
Malik Baktiyarov <malikbakt@gmail.com>
2012-06-03 02:08:58 +0400

    betting

--- src/betting.erl
+++ src/betting.erl
@@ -1,1 +1,29 @@
 -module(betting).
+
+antes(Context) ->
+  Players = player:get_active(Deal#deal.players),
+  Context#context{
+    deal = post_antes(Deal, Context#context.ante, Players)
+  }.
+
+post_antes(Deal, []) ->
+  Deal;
+
+post_antes(Deal, [Player | Left]) ->
+  Ante = bet:sizing(Deal, ?ANTE),
+  post_antes(forced_bet(Deal, Player, Ante), Left).
+
+blinds(Deal) ->
+  Deal1 = post_blind(Deal, ?SMALL_BLIND, ?SB),
+  post_blind(Deal1, ?BIG_BLIND, ?BB).
+
+post_blind(Deal, Type, Position) ->
+  Amount = bet:sizing(Deal, Type),
+  Player = position:get(Deal, Position),
+  forced_bet(Deal, Player, Amount).
+
+forced_bet(Deal, Player, Amount) ->
+  Deal.
+
+big_bets(Deal) ->
+  ok.
